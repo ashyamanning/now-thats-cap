c944f46085364aebba9cdc6eba475ca7c0496b19
Marvin <marvinthompson@pursuit.org>
2021-02-13 16:50:54 -0500

    setting up form skeleton

---
+++ black-facts/src/features/MenuPage/Menu.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+const Menu = () => {
+    const history = useHistory()
+    // the actual menu page 
+    // child components will be rendered in the return below!
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default Menu;
---
+++ black-facts/src/features/MenuPage/MultiplayerTeamNameForm.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const MultiplayerTeamNameForm = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+
+        </form>
+    )
+}
+
+export default MultiplayerTeamNameForm
