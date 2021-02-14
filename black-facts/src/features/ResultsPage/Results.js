d3c33ff574af647447ad3794cc07391a348282f2
bsihame <sihamebazi@pursuit.org>
2021-02-14 18:04:57 -0500

    change Result js file

--- black-facts/src/features/ResultsPage/Results.js
+++ black-facts/src/features/ResultsPage/Results.js
@@ -1,9 +1,39 @@
 import React from "react";
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function Results() {
-  return(
-    <div>
-     <h1>Results Page</h1> 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    // justifyContent: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-around',
+    paddingTop: '10%',
+    '& > *': {
+      width: theme.spacing(80),
+      height: theme.spacing(60),
+    },
+  },
+}));
+
+
+
+export default function Results(props) {
+
+  const classes = useStyles();
+    return (
+      <div className={classes.root}>
+      <Paper elevation={3} >
+        <Paper elevation={10}>
+          <h2>Winner is: Test</h2>
+        </Paper>
+        <Paper elevation={3}>
+          <h2>What can Be</h2>
+        </Paper>
+      </Paper>
     </div>
   )
+  
 }
+
