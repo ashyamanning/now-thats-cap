e4213f959960cf5485f7f58c70dca595343ef74d
Marvin <marvinthompson@pursuit.org>
2021-02-15 16:26:16 -0500

    working on solo team form modal

--- black-facts/src/features/MenuPage/SoloTeamNameForm.js
+++ black-facts/src/features/MenuPage/SoloTeamNameForm.js
@@ -12,9 +12,26 @@ const SoloTeamNameForm = () => {
         // push to game screen
     }
     return (
-        <form onSubmit={handleSubmit}>
-
-        </form>
+        <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLongTitle">Solo Team Game</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={handleSubmit}>
+              <input className="form-control soloNameInput" type="text" placeholder="Player Name"/>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-primary">Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
     )
 }
 
