2b524add593871626df142f5bf39114c8aeeb4fa
Ashya <ashyamanning@pursuit.org>
2021-02-16 18:54:56 -0500

    idk doing stuff

--- black-facts/src/features/MenuPage/MenuButton.js
+++ black-facts/src/features/MenuPage/MenuButton.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from "react-router-dom";
 
 const MenuButton = ({ description }) => {
+  let history = useHistory();
+  
+  const handleMenuButtonRedirect = () => {
+    history.push("/play");
+  };
 
   return (
-    <button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#exampleModalCenter">
+    <button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#exampleModalCenter" onClick={handleMenuButtonRedirect}>
      
      {description.type}
       </button>
