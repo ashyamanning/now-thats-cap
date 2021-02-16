b1b6bb47355a39a8e026542b2dd63e9f32da9267
jenama <johanneenama@gmail.com>
2021-02-15 21:10:18 -0500

    adding questions

--- black-facts/src/features/GamePage/Questions.js
+++ black-facts/src/features/GamePage/Questions.js
@@ -1,26 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import foodData from "../../data/foodData";
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
-class Questions {
 
-  render() {
-    return (
-      <div className="question-prompt">
-        <div className="modal">
-          {foodData.map((food, i) => {
-            {
-              console.log(food.question);
-            }
-            return (
-              <div className="questions">
-                <h3>{food.question}</h3>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function Questions() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  return (
+    
+    <div>
+      <button type="button" onClick={handleOpen}>
+        react-transition-group
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+              {console.log(foodData[0].question)}
+                <h2 id="transition-modal-title">{foodData[0].question}</h2>
+                <div>
+                  {foodData[0].options.map((item, i) => {
+                    return (
+                      <div> 
+                        <p>{item}</p>
+                      </div>
+                    )
+                  })}
+                </div>
+              
+      
+            
+            {/* <p id="transition-modal-description">react-transition-group animates me.</p> */}
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
 }
 
-export default Questions;
+
+
