f4baf5160815b1b352f95df16bfcee53ad53ab6b
Ashya <ashyamanning@pursuit.org>
2021-02-17 23:49:07 -0500

    adding popculture data

--- black-facts/src/data/popcultureData.js
+++ black-facts/src/data/popcultureData.js
@@ -1,5 +1,327 @@
 export const popcultureData = [
-
+    {
+        id: 1,
+        type: "multiple choice",
+        question: "‘XO Tour Lif3’ & the ‘Futsal Shuffle’ are songs belonging to which rapper?",
+        options: ["Rae Sremmurd", "Lil Uzi Vert", "Lil Yachty", "Trippie Red"],
+        answer: "Rae Sremmurd"
+    },
+    {
+        id: 2,
+        type: "multiple choice",
+        question: "This two-time Academy award winner and three-time Screen Guild Award winner is famous for his many dynamic roles, including starring in ‘Moonlight’ & as Colonel Boggs in ‘The Hunger Games’",
+        options: ["Mershala Ali", "Wesley Snipes", "Idris Alba", "Forest Whitaker"],
+        answer: "Mershala Ali"
+    },
+    {
+        id: 3,
+        type: "multiple choice",
+        question: "Which AfroBeats-Alte artist featured on Wizkid’s “Essence” from his Made in Lagos album?",
+        options: ["Burna Boy", "Tems", "Davido", "Mayorkun"],
+        answer: "Tems"
+    },
+    {
+        id: 4,
+        type: "multiple choice",
+        question: "Which UK musical group dropped the hits “Options”, “Lupita” and “Kate Winslet” in 2020?",
+        options: ["NSG", "Krept & Konan", "Young T & Bugsey", "Coldplay"],
+        answer: "NSG"
+    },
+    {
+        id: 5,
+        type: "multiple choice",
+        question: "Drake has collaborated with all the following UK Grime artists except for who?",
+        options: ["Stormzy", "Skepta", "Darkoo", "Wiley"],
+        answer: "Darkoo"
+    },
+    {
+        id: 6,
+        type: "multiple choice",
+        question: "Which popular rap artist played “Modie” in the UK show Top Boy?",
+        options: ["Santan Dave", "Kano", "AJ Tracey", "J Hus"],
+        answer: "Santan Dave"
+    },
+    {
+        id: 7,
+        type: "multiple choice",
+        question: "Idris Elba was NOT a part of this movie.",
+        options: ["The Jungle Book", "Daddy’s Girls", "Obsessed", "The Wire"],
+        answer: "The Wire"
+    },
+    {
+        id: 8,
+        type: "multiple choice",
+        question: "The following popular shows are based on books/comics/manga except for?",
+        options: ["The Walking Dead", "Hunter x Hunter", "Lupin", "Stranger Things"],
+        answer: "Stranger Things"
+    },
+    {
+        id: 9,
+        type: "multiple choice",
+        question: "This popular crime show did not stop filming in 2020?",
+        options: ["Law & Order: SVU", "Criminal Minds", "NCIS", "How to Get Away With Murder"],
+        answer: "Law & Order: SVU"
+    },
+    {
+        id: 10,
+        type: "multiple choice",
+        question: "Tupac Shakur went to what school?",
+        options: ["Julliard", "Baltimore School of Performing Arts", "Berkeley College", "Performing Arts and Technology"],
+        answer: "Baltimore School of Performing Arts"
+    },
+    {
+        id: 11,
+        type: "multiple choice",
+        question: "What are the names of Kim Kardashian and Kanye West’s kids?",
+        options: ["North, Sir, China and Sam", "Dakota, Saint, Sarah, and Justice", "North, Saint, Chicago and Psalm", "North, Hope, Brooklyn, and Psalm"],
+        answer: "North, Saint, Chicago and Psalm"
+    },
+    {
+        id: 12,
+        type: "multiple choice",
+        question: "What is the name of Michelle Obama’s 2018 memoir?",
+        options: ["Becoming", "Acts of a True Lady", "Michelle Obama", "Finally First Lady"],
+        answer: "Becoming"
+    },
+    {
+        id: 13,
+        type: "multiple choice",
+        question: "Which actor voiced both Darth Vader and The Lion King’s Mufasa?",
+        options: ["Keith David", "Dennis Haysbert", "James Earl Jones", "Morgan Freeman"],
+        answer: "James Earl Jones"
+    },
+    {
+        id: 14,
+        type: "multiple choice",
+        question: "What modern-day item made a cameo in the final season of Game of Thrones?",
+        options: ["a car", "an iPhone", "a jet", "A Starbucks cup"],
+        answer: "A Starbucks cup"
+    },
+    {
+        id: 15,
+        type: "multiple choice",
+        question: "What does the acronym “smh” stand for?",
+        options: ["Stop my horse", "Shaking my hips", "Shaking my head", "Shout my honor"],
+        answer: "Shaking my head"
+    },
+    {
+        id: 16,
+        type: "multiple choice",
+        question: "Who sings the song “Say so” that’s behind the popular TikTok dance?",
+        options: ["Saweetie", "Doja Cat", "City Girls", "Megan Thee Stallion"],
+        answer: "Doja Cat"
+    },
+    {
+        id: 17,
+        type: "multiple choice",
+        question: "What day is Star Wars day?",
+        options: ["May 5", "Sept 6", "May 4", "April 3"],
+        answer: "May 4"
+    },
+    {
+        id: 18,
+        type: "multiple choice",
+        question: "Which pop star is the godmother to both of Elton John’s sons?",
+        options: ["Lady Gaga", "Adele", "Drew Barrymore", "Celine Dion"],
+        answer: "Lady Gaga"
+    },
+    {
+        id: 19,
+        type: "multiple choice",
+        question: "What is Rihanna’s full name?",
+        options: ["Raven Fenty", "Rihanna Jenkins", "Robyn Fenty", "Royal Brown"],
+        answer: "Robyn Fenty"
+    },
+    {
+        id: 20,
+        type: "multiple choice",
+        question: "What did Mandalorian fans name “The Child” in the Disney+ series?",
+        options: ["Yadel", "Gorgu", "Baby Yoda", "Jar-Jar"],
+        answer: "Baby Yoda"
+    },
+    {
+        id: 21,
+        type: "multiple choice",
+        question: "Who created the viral “Renegade” dance on TikTok?",
+        options: ["Liza Koshy", "Charli D'amelio", "Annie LeBlanc", "Jalaiah Harmon"],
+        answer: "Jalaiah Harmon"
+    },
+    {
+        id: 22,
+        type: "multiple choice",
+        question: "Who was the first African-American housewife on “The Housewives of Beverly Hills”?",
+        options: ["Garcelle Beauvais", "Eboni Williams", "Cynthia Bailey", "Kandi Burruss"],
+        answer: "Garcelle Beauvais"
+    },
+    {
+        id: 23,
+        type: "multiple choice",
+        question: "Who was Matt Lauer’s official replacement on the TODAY show?",
+        options: ["Ann Curry", "Hoda Kotb", "Savannah Guthrie", "Stephanie Ruhle"],
+        answer: "Hoda Kotb"
+    },
+    {
+        id: 24,
+        type: "multiple choice",
+        question: "In 2020, Netflix aired a reality show that required participants to remain celibate. What was it called?",
+        options: ["Saving Grace", "Enjoy, But Not Too Much", "Too Hot To Handle", "30 Year Old Virgins"],
+        answer: "Too Hot To Handle"
+    },
+    {
+        id: 25,
+        type: "multiple choice",
+        question: "Which tech entrepreneur named his son X Æ A-12?",
+        options: ["Elon Musk", "Larry Ellison", "Mark Zuckerberg", "Jeff Bezos"],
+        answer: "Elon Musk"
+    },
+    {
+        id: 26,
+        type: "multiple choice",
+        question: "What was the name of Meghan Markle’s lifestyle blog before she married into the royal family?",
+        options: ["The Catch", "Brave Waters", "The Tig", "Adventures of Meghan"],
+        answer: "The Tig"
+    },
+    {
+        id: 27,
+        type: "multiple choice",
+        question: "Which hard seltzer did America face a shortage of in 2019?",
+        options: ["Michelob Ultra", "Truly", "White Claw", "High Moon"],
+        answer: "White Claw"
+    },
+    {
+        id: 28,
+        type: "multiple choice",
+        question: "What is the name of the alternate dimension in Stranger Things?",
+        options: ["The Dark", "The Upside Down", "The Other Side", "The Plunder"],
+        answer: "The Upside Down"
+    },
+    {
+        id: 29,
+        type: "multiple choice",
+        question: "Who was the first African-American man to achieve EGOT status?",
+        options: ["Will Smith", "John Legend", "Jay-Z", "Common"],
+        answer: "John Legend"
+    },
+    {
+        id: 30,
+        type: "multiple choice",
+        question: "Who played Rachel Zane on the legal drama Suits?",
+        options: ["Zoe Saldana", "Nathalie Emmanuel", "Zoe Kravitz", "Meghan Markle"],
+        answer: "Meghan Markle"
+    },
+    {
+        id: 31,
+        type: "multiple choice",
+        question: "What is James St. Patrick’s nickname on the show Power?",
+        options: ["Ghost", "Shadow", "Midnight", "Don"],
+        answer: "Ghost"
+    },
+    {
+        id: 32,
+        type: "multiple choice",
+        question: "Which popular rap artist’s real name is Nayvadius Wilburn?",
+        options: ["Lil Uzi Vert", "Future", "Lil Wayne", "21 Savage"],
+        answer: "Future"
+    },
+    {
+        id: 33,
+        type: "multiple choice",
+        question: "Who was the first person of color to win a Grand Slam title?",
+        options: ["Althea Gibson", "Arthur Ashe", "Ora Washington", "Serena Williams"],
+        answer: "Althea Gibson"
+    },
+    {
+        id: 34,
+        type: "multiple choice",
+        question: "Who is the most popular celebrity on social media (with the most fans on Facebook and Twitter)?",
+        options: ["Beyonce", "Oprah Winfrey", "Rihanna", "Alicia Keys"],
+        answer: "Rihanna"
+    },
+    {
+        id: 35,
+        type: "multiple choice",
+        question: "Who was the first female athlete to be featured on the cover of Sports Illustrated?",
+        options: ["Florence Griffith Joyner", "Jackie Joyner Kersee", "Sheryl Swoopes", "Althea Gibson"],
+        answer: "Jackie Joyner Kersee"
+    },
+    {
+        id: 36,
+        type: "multiple choice",
+        question: "Which black comedian holds the longest run on Saturday Night Live?",
+        options: ["Kenan Thompson", "Chris Rock", "Maya Rudolph", "Eddie Murphy"],
+        answer: "Eddie Murphy"
+    },
+    {
+        id: 37,
+        type: "multiple choice",
+        question: "In which U.S. city is the show “Martin” based in?",
+        options: ["Chicago", "Detroit", "Kansas City", "New York City"],
+        answer: "Detroit"
+    },
+    {
+        id: 38,
+        type: "multiple choice",
+        question: "Who has never been in a Tyler Perry movie?",
+        options: ["Alfre Woodard", "Taraji P. Henson", "Regina Hall", "Gabrielle Union"],
+        answer: "Regina Hall"
+    },
+    {
+        id: 39,
+        type: "multiple choice",
+        question: "Who did Jada Pinkett-Smith reveal she had an “entanglement” with?",
+        options: ["August Alsina", "", "", ""],
+        answer: "August Alsina"
+    },
+    {
+        id: 40,
+        type: "multiple choice",
+        question: "Who was the first Black female gymnast to win a gold medal at the Olympics?",
+        options: ["Gabby Douglas", "Dominique Dawes", "Betty Okino", "Debi Thomas"],
+        answer: "Dominique Dawes"
+    },
+    {
+        id: 41,
+        type: "multiple choice",
+        question: "What TV series is credited as the first weekly series to depict a Black woman in a non-stereotypical lead role?",
+        options: ["Girlfriends", "Julia", "227", "A Different World"],
+        answer: "Julia"
+    }
+    // ,
+    // {
+    //     id: 42,
+    //     type: "multiple choice",
+    //     question: "",
+    //     options: ["", "", "", ""],
+    //     answer: ""
+    // },
+    // {
+    //     id: 43,
+    //     type: "multiple choice",
+    //     question: "",
+    //     options: ["", "", "", ""],
+    //     answer: ""
+    // },
+    // {
+    //     id: 44,
+    //     type: "multiple choice",
+    //     question: "",
+    //     options: ["", "", "", ""],
+    //     answer: ""
+    // },
+    // {
+    //     id: 45,
+    //     type: "multiple choice",
+    //     question: "",
+    //     options: ["", "", "", ""],
+    //     answer: ""
+    // },
+    // {
+    //     id: 46,
+    //     type: "multiple choice",
+    //     question: "",
+    //     options: ["", "", "", ""],
+    //     answer: ""
+    // }
 ]
 
 export default popcultureData;
